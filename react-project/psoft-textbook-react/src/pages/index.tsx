afe4ed8a7ee9a0a5ec60ec0354e88075063898b0
Siggy Nazimowitz <thewotter@icloud.com>
2024-06-28 18:55:52 -0400

    hey guys

--- react-project/psoft-textbook-react/src/pages/index.tsx
+++ react-project/psoft-textbook-react/src/pages/index.tsx
@@ -29,7 +29,6 @@ export default function Index() {
       ]
     }
     const targetURL = "https://clownfish-app-mfank.ondigitalocean.app/compile";
-    const proxyURL = "https://api.allorigins.win/get?url=" + encodeURIComponent("https://clownfish-app-mfank.ondigitalocean.app/compile");
     var request = new XMLHttpRequest();
     request.onreadystatechange= function () {
       if (request.readyState==4) {
@@ -67,9 +66,11 @@ export default function Index() {
         }
       }
     };
-    request.open("POST", proxyURL);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send(JSON.stringify(obj));
+    request.open("POST", targetURL);
+    const blob = new Blob([JSON.stringify(obj)], {
+      type: "application/json",
+    });
+    request.send(blob);
   };
 
 
