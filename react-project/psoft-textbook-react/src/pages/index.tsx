cc3170998d2075047eb3283737a435eb28e6b230
Siggy Nazimowitz <thewotter@icloud.com>
2024-06-29 11:46:31 -0400

    testing

--- react-project/psoft-textbook-react/src/pages/index.tsx
+++ react-project/psoft-textbook-react/src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Index() {
     const targetURL = "https://clownfish-app-mfank.ondigitalocean.app/compile";
     const proxyURL = "https://api.allorigins.win/get?url=" + encodeURIComponent("https://clownfish-app-mfank.ondigitalocean.app/compile");
     let get = async () => {
-    let response = await fetch(proxyURL, {
+    let response = await fetch("http://localhost/compile", {
       body: blob,
       method: "POST",
   })
