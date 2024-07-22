6720c532ac0481bd4bda58025e81254cbf2ec0d5
Siggy Nazimowitz <thewotter@icloud.com>
2024-07-22 13:56:21 -0400

    cleanup

--- react-project/psoft-textbook-react/src/pages/CFGCanvas.tsx
+++ react-project/psoft-textbook-react/src/pages/CFGCanvas.tsx
@@ -1,6 +1,5 @@
 import Navbar from "../components/Navbar";
 import { Toolbar } from 'primereact/toolbar';
-import "../components/CFGSidebar.css"
 import React, { MouseEvent, useState, useEffect, useRef } from 'react';
 import { ReactFlow, ReactFlowProvider, getViewportForBounds, useReactFlow } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
